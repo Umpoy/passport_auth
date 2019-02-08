ddc9960f49995c1693837da952f7cf5f3515bbc5
Umpoy <iumpoy@yahoo.com>
2019-02-08 13:49:44 -0800

    created index route

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+require('./routes/index')(app);
+
 app.listen(port, () => {
     console.log("Server running on localhost: " + port)
 })
